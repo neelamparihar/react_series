c046cddf8be26298eda82f8a24059295d42c65c4
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-09-28 06:43:58 +0000

    please commit

--- tailwindprops_tut/src/App.js
+++ tailwindprops_tut/src/App.js
@@ -1,18 +1,14 @@
 
 import './App.css';
-
+import Card from './components/Card.js'
 function App() {
   return (
     <>
-    
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://images.pexels.com/photos/18418223/pexels-photo-18418223/free-photo-of-city-buildings-building-construction.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </div>
-</a>
-
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Card username="Neelam" />
+    <Card username="Nidhi" />
     </>
   );
 }
